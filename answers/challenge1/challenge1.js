992778a7298054ea17c0cd328bf4828a69fe2183
Jorge Morales <jorge_mlg@hotmail.com>
2021-10-05 23:06:52 -0400

    add shuffle function

--- answers/challenge1/challenge1.js
+++ answers/challenge1/challenge1.js
@@ -22,6 +22,24 @@ $(document).ready(function() {
     setTimeout(nextTrial,iti); 
 });
 
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+  }
+
 function createTrials () {
     targetSide = ["left","right"];
     distractorColor = ["green", "red"];
